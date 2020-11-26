6e8f17701e7fcc0d03bd5a7a1b3cfd4efb501382
omakhlas <omarmakhlas@gmail.com>
2020-11-26 13:38:49 +0100

    adding negative number & numbers bigger than 1000

--- src/main/java/io/github/newlight77/bootstrap/Calculator.java
+++ src/main/java/io/github/newlight77/bootstrap/Calculator.java
@@ -4,7 +4,18 @@ import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 public class Calculator {
 
-    public long add(String numbers) throws NotImplementedException {
-        throw new NotImplementedException();
+    public int add(String numbers) throws Exception{
+        int som = 0;
+        if(numbers != null && !numbers.isEmpty()) {
+            String[] arr = numbers.split(",|\\\n");
+            for(String s : arr) {
+                int val = Integer.parseInt(s);
+                if(val>1000) continue;
+                if(val<0) throw new Exception();
+                som+= val;
+            }
+            return som;
+        }
+        return som;
     }
 }
--- src/test/java/io/github/newlight77/bootstrap/AppMainTest.java
+++ src/test/java/io/github/newlight77/bootstrap/AppMainTest.java
@@ -1,6 +1,7 @@
 package io.github.newlight77.bootstrap;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -19,8 +20,40 @@ public class AppMainTest {
     }
 
     @Test
-    void shouldReturn0WhenNumbersIsEmpty() {
+    void shouldReturn0WhenNumbersIsEmpty() throws Exception{
         assertEquals(0,calculator.add(""));
+    }
+
+    @Test
+    void shouldReturn1WhenNumbersIs1() throws Exception{
+        assertEquals(1,calculator.add("1"));
+    }
+
+    @Test
+    void shouldReturn3WhenNumbersIs1_2() throws Exception{
+        assertEquals(3,calculator.add("1,2"));
+    }
+
+    @Test
+    void shouldReturn3WhenNumbersIs1_n_2() throws Exception{
+        assertEquals(3,calculator.add("1\n2"));
+    }
+
+    @Test
+    void shouldReturn6WhenNumbersIs1_n2_3() throws Exception{
+        assertEquals(6,calculator.add("1\n2,3"));
+    }
+
+    @Test
+    void shouldThrowExceptionWhenNumbersContainsNegativeNb(){
+        assertThrows(Exception.class, () -> {
+            calculator.add("1,2,-3");
+        });
+    }
+
+    @Test
+    void shouldReturn2WhenNumbersIs1_1001() throws Exception{
+        assertEquals(2,calculator.add("2,1001"));
     }
 
 }
