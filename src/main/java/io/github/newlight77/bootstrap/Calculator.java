e84c07ec7ced0cf978d8c2e9ec65618781cad4b9
omakhlas <omarmakhlas@gmail.com>
2020-11-26 12:50:12 +0100

    [add] travis.yml

---
+++ src/main/java/io/github/newlight77/bootstrap/Calculator.java
@@ -0,0 +1,10 @@
+package io.github.newlight77.bootstrap;
+
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+public class Calculator {
+
+    public long add(String numbers) throws NotImplementedException {
+        throw new NotImplementedException();
+    }
+}
--- src/test/java/io/github/newlight77/bootstrap/AppMainTest.java
+++ src/test/java/io/github/newlight77/bootstrap/AppMainTest.java
@@ -2,13 +2,25 @@ package io.github.newlight77.bootstrap;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class AppMainTest {
+    Calculator calculator;
+    @BeforeEach
+    void init() {
+        calculator = new Calculator();
+    }
 
     @Test
     void myTest() {
         assertEquals(true, true);
+    }
+
+    @Test
+    void shouldReturn0WhenNumbersIsEmpty() {
+        assertEquals(0,calculator.add(""));
     }
 
 }
